11c3d2c7bf7f5c0cd9db714d903d29594bf0e57a
haminata <amtavcc@hotmail.com>
2017-11-05 08:09:03 +0000

    Lab03 essentials: square description

--- exercise/src/lab03/Square.java
+++ exercise/src/lab03/Square.java
@@ -1,17 +1,32 @@
 package lab03;
 
+/**
+ * created by Haminata
+ */
 public class Square {
     public double x;
 
+    /**
+     * constructor of the square that takes parameters
+     * @param x is a variable of the side inside a inside a parameter
+     */
     public Square (double x){
         this.x = x;
     }
 
+    /**
+     * method that calculates the area of the square
+     * @return the area
+     */
     public double calcArea(){
        double area = x * x;
         return area;
     }
 
+    /**
+     * 2 instances taking parameter values
+     * @param args their area values
+     */
     public static void main (String [] args){
         Square sq1 = new Square(10);
         System.out.println(sq1.calcArea());
