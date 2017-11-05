00288f0cf06217fdba9c9e42ecaf0ff09a5e7114
haminata <amtavcc@hotmail.com>
2017-11-05 16:11:05 +0000

    Lab03 essentials: started counter

--- exercise/src/lab03/Counter.java
+++ exercise/src/lab03/Counter.java
@@ -1,5 +1,8 @@
 package lab03;
 
+/**
+ * created by Haminata
+ */
 public class Counter {
     private int count;
     private int max;
@@ -8,4 +11,37 @@ public class Counter {
         this.count = 0;
         this.max = 10;
     }
-}
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setCount(int n) {
+        this.count = n;
+    }
+
+    public void setMax(int n) {
+        this.max = n;
+    }
+
+    public void increase(){
+        this.count = this.count +1;
+
+    }
+
+    public void decrease(){
+        this.count = this.count -1;
+    }
+
+    public void reset(){
+       this.count = 0;
+        System.out.println("Counter Reset!");
+    }
+   public String toString(){
+        return ("Max value: " + this.max + ", " + "Count:" + count);
+    }
+     }
--- exercise/src/lab03/CounterTest.java
+++ exercise/src/lab03/CounterTest.java
@@ -1,4 +1,26 @@
 package lab03;
 
+/**
+ * created by Haminata
+ */
 public class CounterTest {
+    public static void main(String[] args) {
+        Counter count1 = new Counter();
+        count1.setCount(6);
+
+        System.out.println("Current count is " + count1.getCount());
+        System.out.println("Current max is " + count1.getMax());
+
+        count1.increase();
+        System.out.println(count1);
+
+        count1.decrease();
+        System.out.println(count1);
+
+        count1.reset();
+        System.out.println(count1);
+
+        // Start of the
+
+    }
 }
