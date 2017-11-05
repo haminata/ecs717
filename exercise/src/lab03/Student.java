a4e7abd273802a22295ae59bb1407ccba026678d
haminata <amtavcc@hotmail.com>
2017-11-05 16:17:45 +0000

    lab03 exercise 16(a and b) create class 2 instances of a class create a constructor and one method

--- exercise/src/lab03/Student.java
+++ exercise/src/lab03/Student.java
@@ -1,24 +1,37 @@
 package lab03;
 
 /**
- * Created by haminata on 04/11/2017.
+ * Created by Haminata on 04/11/2017.
  *
  */
 public class Student {
+    //is a class called Student with attributes
     public String name;
     public String gender;
     public int yearOfBirth;
 
+    /**
+     * constructor with parameters - called at creation time to initialise attributes
+     * @param name represent the first and last name of the student
+     * @param gender gender of the student
+     * @param yearOfBirth year of birth of the student
+     */
     public Student(String name, String gender, int yearOfBirth){
         this.name = name;
         this.gender = gender;
         this.yearOfBirth = yearOfBirth;
     }
 
+    /**
+     * method defines the student have been registered
+     */
     public void register(){
         System.out.println("You have been registered!");
     }
 
+    /**
+     * @return string representation of the class
+     */
     public String toString(){
         return "Student(name: " + name + ", gender: " + gender + ", yearOfBirth: " + yearOfBirth + ")";
     }
--- exercise/src/lab03/StudentTest.java
+++ exercise/src/lab03/StudentTest.java
@@ -11,12 +11,14 @@ import java.util.Date;
 public class StudentTest {
     public static void main(String[] args) {
 
-        Student student1 = new Student("Haminata Camara" ,"female", 1990);
+        // create a student using my name, gender and year of birth
+        Student student1 = new Student("Haminata Camara", "female", 1990);
         System.out.println(student1);
         student1.register();
 
         System.out.println();
 
+        // second student using exercise specification
         Student student2 = new Student("Rick Sanchez", "male", 1972);
         System.out.println(student2);
         student2.register();
