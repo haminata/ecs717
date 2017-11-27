ccbd2a952f8813be9c1a70b49192b1d57825bb57
haminata <amtavcc@hotmail.com>
2017-11-27 23:22:01 +0000

    lab07 average

---
+++ exercise/src/lab07/AverageN.java
@@ -0,0 +1,31 @@
+package lab07;
+import java.util.Scanner;
+
+/**
+ * Created by haminata on 27/11/2017.
+ */
+public class AverageN {
+    int[] nu;
+
+    public static int calAve(int n){
+
+     return n;
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Insert numbers: " + n);
+
+    }
+}
