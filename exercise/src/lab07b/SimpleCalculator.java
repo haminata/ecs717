20a77a301b7e9d12bc63122be521a41c0b3b77fa
haminata <amtavcc@hotmail.com>
2017-12-04 11:42:00 +0000

    Lab07b essentials

--- exercise/src/lab07b/SimpleCalculator.java
+++ exercise/src/lab07b/SimpleCalculator.java
@@ -1,6 +1,7 @@
 package lab07b;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by haminata on 03/12/2017.
@@ -10,5 +11,12 @@ public class SimpleCalculator extends JFrame{
     public SimpleCalculator(){
         super();
 
+
     }
+    public void addComponentsToPane(final Container pane) {
+
+        final JPanel compsToExperiment = new JPanel();
+        compsToExperiment.setLayout(experimentLayout);
+        JPanel controls = new JPanel();
+        controls.setLayout(new GridLayout(2,3));
 }
